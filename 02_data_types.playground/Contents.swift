e2cf5c72b51682d829350fba5b3376eaa93d3b20
Minsoo Kwak <marcosdeseul@gmail.com>
2017-03-02 20:00:33 +0800

    Add data type
    - explained the difference between implicit and explicit
    - convert string/integer/double

---
+++ 02_data_types.playground/Contents.swift
@@ -0,0 +1,44 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// implicit
+var str = "Hello, playground"
+//str = 3
+//var & let
+
+var number = 1
+
+// explict
+var str2:String = "hello"
+//var str3:String = 1
+
+var number2:Int = 2
+//var number2:Int = "playground"
+
+
+// convert string to integer
+10 + 11
+"10" + "11"
+//"10" + 11
+var numStr = "10"
+//var numStr = "hello"
+Int(numStr)
+
+
+// convert integer to string
+var number3 = 5
+var str3 = "hello"
+"\(number3)"
+"\(str3)! \(number3)"
+String(number3)
+
+// integer
+3 / 2
+
+// double / float
+3.0 / 2.0
+
+// double to integer
+Int(3.0 / 2.0)
+
