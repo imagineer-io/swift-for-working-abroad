6b22c1e3b8c5743eeb0fc3f27642212cfd8a7a1f
Minsoo Kwak <marcosdeseul@gmail.com>
2017-03-03 14:33:26 +0800

    Add set and dictionary explanation

--- 03_collection_types.playground/Contents.swift
+++ 03_collection_types.playground/Contents.swift
@@ -4,6 +4,7 @@ import UIKit
 
 var str = "Hello, playground"
 
+//
 // array (list)
 var someInts = [Int]()
 someInts.append(1)
@@ -17,9 +18,41 @@ someStrings[1] = "hii"
 someStrings
 someStrings[0...2]
 
+//
 // tuple
 var tupleInts = (1, 2, 3)
 //tupleInts.append(4)
 
+//
 // set
+var setStrings = Set<String>()
+setStrings.insert("how")
+setStrings.insert("what")
+setStrings = ["who", "who"]
+setStrings.count
+
+// 1) 중복이 안된다
+// 2) 순서가 없다
+// 3) 집합 규칙을 사용할 수 있다.
+
+//
 // dictionary (hash map)
+var dictStrings = [String: String]()
+dictStrings["usa"] = "hello"
+dictStrings["spain"] = "hola"
+dictStrings
+dictStrings["usa"]
+dictStrings.count
+
+
+
+
+
+
+
+
+
+
+
+
+
