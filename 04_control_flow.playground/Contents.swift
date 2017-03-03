a946f31b813371b575600de786893634577e4e9c
Minsoo Kwak <marcosdeseul@gmail.com>
2017-03-03 16:39:52 +0800

    Add if-statement
    - explained boolean and if
    
    TODO:
    - nil
    - optional
    - optional binding

---
+++ 04_control_flow.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//var str = "Hello, playground"
+//var & let
+let thisIsTrue = false
+if thisIsTrue {
+    print("this is true")
+} else {
+    print("this is false")
+}
+
+if 1 == 2 {
+    print("true")
+} else {
+    print("false")
+}
+
+let num1 = 1
+let num2 = 5
+
+if num2 > num1 {
+    print("num2 is greater than num1")
+}
